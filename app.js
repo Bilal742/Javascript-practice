596cd63ff1ad5f9831ae79accbd93294dcbaecfe
Bilal <your_email@example.com>
2025-12-16 04:44:48 -0800

    feat: initial commit with JavaScript practice files

--- app.js
+++ app.js
@@ -425,7 +425,7 @@ let Obj = {
 // console.log(array[3]);
 // console.log(array[4]);
 // console.log(array[5]);
-// console.log(array[6]);
+// console.log(array[6]);  
 
 // console.log("The length of arrays " + array.length);
 
