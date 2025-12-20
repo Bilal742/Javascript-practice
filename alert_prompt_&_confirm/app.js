b16062ecd350f05db6d3df71a59b6e115112f94a
Bilal <your_email@example.com>
2025-12-19 23:06:06 -0800

    JavaScript practice files update

--- alert_prompt_&_confirm/app.js
+++ alert_prompt_&_confirm/app.js
@@ -1,3 +1,19 @@
 console.log("Hello world");
 
 alert("Enter the value of a");
+let a = prompt("Enter a here", "304");
+a = Number.parseInt(a);
+
+let write = confirm("Do you want to write in to the page");
+
+if (write) {
+    document.writeln(`<h1>${a}</h1>`);
+}
+
+else {
+    document.writeln("Please allow me to write");
+};
+
+write ?
+    document.writeln(`<h1>${a}</h1>`)
+    : document.writeln("Please allow me to write");
