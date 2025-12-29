af0254d06cc384bd3e06d95a6eeaa8bc870293f3
Bilal <your_email@example.com>
2025-12-29 13:27:59 -0800

    ✨ Update

---
+++ Walking_the_DOM/app.js
@@ -0,0 +1,9 @@
+// console.log(document.body);
+// console.log(document.documentElement);
+// document.title = "Code with harry";
+console.log(typeof(document.body));
+
+let box = document.getElementById("box");
+
+console.log(box); 
+
