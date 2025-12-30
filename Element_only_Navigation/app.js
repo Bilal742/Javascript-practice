de384653bc159dcddfc8884bf5a8b3e72b812988
Bilal <your_email@example.com>
2025-12-30 00:06:17 -0800

    🔗 Understand Element only Navigation

---
+++ Element_only_Navigation/app.js
@@ -0,0 +1,9 @@
+console.log("Hello World");
+
+let b = document.body;
+
+// console.log(`First child of b is: ${b.firstChild}`);
+// console.log(`First Element child of b is: ${b.firstElementChild.nodeName}`);
+
+console.log(`First child of b is:`, b.firstChild);
+console.log(`First Element child of b is:`, b.firstElementChild);
