4ccc48f44b45016946a9a81a9d565614e8339e2d
Bilal <your_email@example.com>
2025-12-29 22:16:09 -0800

    ✨ DOM children access practice

--- Accessing_children_of_an_lement/app.js
+++ Accessing_children_of_an_lement/app.js
@@ -1,1 +1,8 @@
 console.log("Hello world");
+
+// console.log(document.body.firstChild);
+// console.log(document.children[0]);
+// console.log(document.body.firstElementChild);
+console.log(childNodes);
+
+
