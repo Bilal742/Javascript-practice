f058b297822b37e659450528da88c5404e046118
Bilal Usman <bilalusman1291@gmail.com>
2026-01-02 11:40:40 +0500

    🔍 Practice DOM searching methods (getElementById, querySelector, etc.)

--- Searching_the_DOM/app.js
+++ Searching_the_DOM/app.js
@@ -1,23 +1,43 @@
-const div = document.getElementById("myDiv");
-console.log(div.innerText);       
+// const div = document.getElementById("myDiv");
+// console.log(div.innerText);       
 
-const paras = document.getElementsByClassName("paras");
-console.log(paras[0].textContent);  
+// const paras = document.getElementsByClassName("paras");
+// console.log(paras[0].textContent);  
 
-//          o o                                           
-//          | |                                           
-//         _L_L_                                          
-//      ❮\/__-__\/❯ Programming isn't about what you know 
-//      ❮(|~o.o~|)❯  It's about what you can figure out   
-//      ❮/ \`-'/ \❯                                       
-//        _/`U'\_                                         
-//       ( .   . )     .----------------------------.     
-//      / /     \ \    | while( ! (succeed=try() ) ) |     
-//      \ |  ,  | /    '----------------------------'     
-//       \|=====|/                                         
-//        |_.^._|                                         
-//        | |"| |                                         
-//        ( ) ( )   Testing leads to failure              
-//        |_| |_|   and failure leads to understanding    
-//    _.-' _j L_ '-._                                     
+// const paras = document.querySelector(".paras");
+// let change = paras.style.color = "red";
+// change = paras.style.background = "black"
+// console.log(change);
+
+const allParas = document.querySelectorAll(".paras");
+// let apply = allParas.style.color = "red";
+// console.log(apply);
+
+allParas.forEach(para => {
+    para.style.color = "red"
+});
+
+let nums = [1, 2, 3];
+
+let double = nums.map(n => n * 2);
+console.log(double); 
+
+
+
+//          o o
+//          | |
+//         _L_L_
+//      ❮\/__-__\/❯ Programming isn't about what you know
+//      ❮(|~o.o~|)❯  It's about what you can figure out
+//      ❮/ \`-'/ \❯
+//        _/`U'\_
+//       ( .   . )     .----------------------------.
+//      / /     \ \    | while( ! (succeed=try() ) ) |
+//      \ |  ,  | /    '----------------------------'
+//       \|=====|/
+//        |_.^._|
+//        | |"| |
+//        ( ) ( )   Testing leads to failure
+//        |_| |_|   and failure leads to understanding
+//    _.-' _j L_ '-._
 //   (___.'     '.___)                                    
