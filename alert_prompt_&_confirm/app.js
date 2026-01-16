99968e60873045bc8d582b38c247062507cfed7e
Bilal Usman <bilalusman1291@gmail.com>
2026-01-17 03:30:32 +0500

    ?? updates files

--- alert_prompt_&_confirm/app.js
+++ alert_prompt_&_confirm/app.js
@@ -1,19 +1,20 @@
-console.log("Hello world");
+// console.log("Hello world");
 
-alert("Enter the value of a");
-let a = prompt("Enter a here", "304");
-a = Number.parseInt(a);
+// alert("Enter the value of a");
+// let a = prompt("Enter a here", "304");
+// a = Number.parseInt(a);
 
-let write = confirm("Do you want to write in to the page");
+// let write = confirm("Do you want to write in to the page");
 
-if (write) {
-    document.writeln(`<h1>${a}</h1>`);
-}
+// if (write) {
+//     document.writeln(`<h1>${a}</h1>`);
+// }
 
-else {
-    document.writeln("Please allow me to write");
-};
+// else {
+//     document.writeln("Please allow me to write");
+// };
 
-write ?
-    document.writeln(`<h1>${a}</h1>`)
-    : document.writeln("Please allow me to write");
+// write ?
+//     document.writeln(`<h1>${a}</h1>`)
+//     : document.writeln("Please allow me to write");
+
